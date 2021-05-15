7241ecf77e836c57f8a78839373156d80556d96a
Conrad Klaus <conrad.y.klaus@campus.tu-berlin.de>
2021-05-16 01:06:53 +0200

    add control for two motors

--- motorController.py
+++ motorController.py
@@ -3,46 +3,57 @@
 import RPi.GPIO as GPIO
 import time
 
-enB = 33
-in4 = 31
-in3 = 29
+
+# info: https://projects.raspberrypi.org/en/projects/robotPID/2
+
+class Motor(object):
+    def __init__(self, en, in_1, in_2):
+        self._en = en
+        self._in_1 = in_1
+        self._in_2 = in_2
+        GPIO.setup(self._en, GPIO.OUT)
+        self._pwm = GPIO.PWM(self._en, 100)
+        self._pwm.start(0)
+        GPIO.setup(self._in_1, GPIO.OUT)
+        GPIO.setup(self._in_2, GPIO.OUT)
+
+    def rotate(self, direction, speed):
+        if direction:
+            GPIO.output(self._in_1, GPIO.HIGH)
+            GPIO.output(self._in_2, GPIO.LOW)
+        else:
+            GPIO.output(self._in_1, GPIO.LOW)
+            GPIO.output(self._in_2, GPIO.HIGH)
+        self._pwm.ChangeDutyCycle(speed)
+
+    def stop(self):
+        GPIO.output(self._in_1, GPIO.HIGH)
+        GPIO.output(self._in_2, GPIO.HIGH)
+        self._pwm.ChangeDutyCycle(0)
+
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(enB, GPIO.OUT)
-GPIO.setup(in3, GPIO.OUT)
-GPIO.setup(in4, GPIO.OUT)
-
-GPIO.output(enB, GPIO.HIGH)
-GPIO.output(in3, GPIO.LOW)
-GPIO.output(in4, GPIO.LOW)
-
-pwm = GPIO.PWM(enB, 100)
-pwm.start(0)
-
-
-def rotate(direction):
-    if direction:
-        GPIO.output(in3, GPIO.HIGH)
-        GPIO.output(in4, GPIO.LOW)
-    else:
-        GPIO.output(in3, GPIO.LOW)
-        GPIO.output(in4, GPIO.HIGH)
-
+m1 = Motor(18, 24, 22)
+m2 = Motor(31, 26, 29)
 
 try:
-    pwm.ChangeDutyCycle(100)
-    rotate(False)
+    m1.rotate(False, 100)
+    m2.rotate(False, 100)
     for i in range(0, 100 + 1):
         if i % 10 == 0:
             print("PWM: {}".format(i))
-        pwm.ChangeDutyCycle(i)
+        m1.rotate(False, i)
+        m2.rotate(False, i)
         time.sleep(0.05)
-    rotate(True)
+    m1.rotate(True, 100)
+    m2.rotate(True, 100)
     while True:
         time.sleep(1)
 
 except KeyboardInterrupt:
     # quit
+    m1.stop()
+    m2.stop()
     GPIO.cleanup()
     print(" exit")
     exit()
